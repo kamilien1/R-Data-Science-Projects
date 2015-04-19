db122f3b210e7f809cc2b9149dd07bc7cedae4eb
kamilien1 <kamilien1@gmail.com>
2015-04-20 00:19:13 +0800

    Added Assignment 2

--- complete.R
+++ complete.R
@@ -12,4 +12,33 @@ complete <- function(directory, id = 1:332) {
     ## ...
     ## where 'id' is the monitor ID number and 'nobs' is the
     ## number of complete cases
+    
+    
+    output_file <- matrix(nrow=0,ncol=2)
+    colnames(output_file)<-c("id","nobs")
+    
+    # to add a new row, do output_file <-rbind(output_file,c(val1,val2))
+    
+    for (i in id) {
+        
+        # sprintf %03d ensures a standard 3 digit length number
+        # taken from https://class.coursera.org/rprog-013/forum/thread?thread_id=324
+        f_name <- paste(directory,"/",sprintf("%03d",i),".csv",sep="")
+        f_read <- read.csv(f_name)
+        
+        
+        # add a row with i and # of complete cases (dim(thingy)[rows])
+        #print(paste("i is",i))
+        #print(paste("complete cases is",dim(complete.cases(f_read))[1]))
+        #print("complete cases is")
+        #print(dim(f_read[complete.cases(f_read),]))[1]
+        
+        output_file <- rbind(output_file,c(i,dim(f_read[complete.cases(f_read),])[1]))
+    
+        
+    }
+    as.data.frame(output_file)
 }
+
+#specdata <- "specdata"
+# complete(specdata,1:5)
--- corr.R
+++ corr.R
@@ -8,4 +8,52 @@ corr <- function(directory, threshold = 0) {
     ## nitrate and sulfate; the default is 0
     
     ## Return a numeric vector of correlations
+    
+    output_file <- {}
+    
+    # for iteration purposes
+    all_files <- list.files(directory)
+    num_files <- length(list.files(directory))
+    
+    # threshold and create a new matrix
+    for (i in all_files){
+        
+        f_name <- paste(directory,"/",i,sep="")
+        f_read <- read.csv(f_name)
+        
+        # output_file <- rbind(output_file,c(i,dim(f_read[complete.cases(f_read),])[1]))
+        
+        num_cases <- dim(f_read[complete.cases(f_read),])[1]
+        
+        # if num_cases > threshold, add to list
+       
+        if (num_cases > threshold) {
+            
+            
+            temp_complete_cases <- complete.cases(f_read)
+            temp_complete_matrix <-f_read[temp_complete_cases,2:3]
+            temp_cor <- cor(temp_complete_matrix[,1],temp_complete_matrix[,2])
+            
+            output_file <- c(output_file, temp_cor)
+            
+            
+        }
+        
+    }
+    
+
+   output_file
 }
+
+cr <- corr("specdata", 150)
+head(cr)
+summary(cr)
+cr <- corr("specdata", 400)
+head(cr)
+summary(cr)
+cr <- corr("specdata", 5000)
+summary(cr)
+length(cr)
+cr <- corr("specdata")
+summary(cr)
+length(cr)
